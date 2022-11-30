1361c789742e5f0999e269f51bb29b800db4a42a
Ike Maldonado <maldonadoike@gmail.com>
2022-11-29 23:41:47 -0800

    refactored scene

--- src/App.jsx
+++ src/App.jsx
@@ -6,70 +6,58 @@ import { MathUtils } from "three";
 import "../src/index.css";
 import { RigidBody, CuboidCollider, Physics } from "@react-three/rapier";
 
-let pos = {
-  x: 0,
-  y: 0,
-  z: 0,
-};
-
 function setRandomPositions() {
+  let pos = {};
   pos.x = MathUtils.randFloat(-5, 5);
   pos.y = MathUtils.randFloat(50, 10);
   pos.z = MathUtils.randFloat(-5, 5);
   return pos;
 }
 
-const Scene = () => {
+let rows = [];
+for (let i = 0; i < 100; i++) {
+  const p = setRandomPositions();
+  rows.push(<Scene key={i} position={p} />);
+}
+
+function Scene({ position }) {
   const boxRef = useRef();
   const bodyRef = useRef();
   const [disabled, setDisabled] = useState(false);
 
-  // // Provides bounce when clicked
-  // const { scale } = useSpring({
-  //   scale: active ? 1.5 : 1,
-  //   config: config.wobbly,
-  // });
-
-  let rows = [];
-  for (let i = 0; i < 100; i++) {
-    setRandomPositions();
-    rows.push(
-      <RigidBody
-        ref={bodyRef}
-        colliders={"cuboid"}
-        restitution={0.1}
-        position={[pos.x, pos.y, pos.z]}
-        key={i}
-      >
-        <Box castShadow receiveShadow ref={boxRef} key={i}>
-          <meshStandardMaterial
-            transparent={disabled ? true : false}
-            opacity={disabled ? 0.1 : 1}
-            attach="material"
-            color="orange"
-          />
-        </Box>
-      </RigidBody>
-    );
-  }
-
-  return <group>{rows}</group>;
-};
+  return (
+    <RigidBody
+      ref={bodyRef}
+      colliders={"cuboid"}
+      restitution={0.1}
+      position={[position.x, position.y, position.z]}
+    >
+      <Box castShadow ref={boxRef}>
+        <meshStandardMaterial
+          transparent={disabled ? true : false}
+          opacity={disabled ? 0.1 : 1}
+          attach="material"
+          color="orange"
+        />
+      </Box>
+    </RigidBody>
+  );
+}
 
 export default function App() {
   return (
-    <Canvas shadows camera={{position: [15,5,5], fov: 75 }}>
+    <Canvas shadows camera={{ position: [15, 5, 5], fov: 75 }}>
+      {/* <fog attach="fog" args={["white", 10, 40]} /> */}
+      <ambientLight intensity={0.5} />
+      <directionalLight
+        intensity={1}
+        castShadow
+        position={[1, 3, 2]}
+        // shadow-mapSize-height={512}
+        // shadow-mapSize-width={512}
+      />
       <Suspense>
         <Physics>
-          <fog attach="fog" args={["white", 0, 40]} />
-          <ambientLight intensity={0.5} />
-          <directionalLight
-            intensity={0.7}
-            castShadow
-            position={[1, 3, 2]}
-            shadow-mapSize-height={512}
-            shadow-mapSize-width={512}
-          />
           <Plane
             receiveShadow
             rotation={[-Math.PI / 2, 0, 0]}
@@ -78,8 +66,7 @@ export default function App() {
           >
             <meshStandardMaterial attach="material" color="white" />
           </Plane>
-          <Scene />
-          <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2.1} />
+          {rows}
           <CuboidCollider
             position={[0, 0, 0]}
             args={[20, 0, 20]}
@@ -87,6 +74,7 @@ export default function App() {
           ></CuboidCollider>
         </Physics>
       </Suspense>
+      <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2.1} />
     </Canvas>
   );
 }
