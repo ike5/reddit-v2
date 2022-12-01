2e2ba3bc9fa352fd5c9d91ddd3dc53a6c8e33c2b
Ike Maldonado <maldonadoike@gmail.com>
2022-12-01 13:27:37 -0800

    we are going to have to go with this: no navbar for now

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -5,15 +5,14 @@ import Footer from "./Footer";
 
 function Grid() {
   return (
-    <div className="container gap-4">
-      <div className="nav">
+    <div className="grid auto-rows-fr grid-cols-4 gap-4">
+      {/* <div className="nav row-span-1 col-start-1 col-end-5">
         <Navbar />
-      </div>
-      <div className="main pl-4">
+      </div> */}
+      <div className="main row-span-4 col-start-1 col-end-5 pl-2 pr-2 pt-2">
         <App />
       </div>
-      <div className="sidebar">Sidebar</div>
-      <div className="footer">
+      <div className="footer row-span-1 col-start-1 col-end-5">
         <Footer />
       </div>
     </div>
