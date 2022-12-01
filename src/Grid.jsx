be0cfb846eecc89156d9abf5aa114852b936619d
Ike Maldonado <maldonadoike@gmail.com>
2022-12-01 15:26:27 -0800

    add resources

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -6,15 +6,12 @@ import Coffee from "./Coffee";
 
 function Grid() {
   return (
-    <div className="grid grid-rows-6 grid-cols-4 gap-4 place-content-center">
-      <div className="row-start-1 row-end-5 col-start-1 col-end-5 pl-2 pr-2 pt-2">
+    <div className="my-container gap-4 place-content-center">
+      <div className="main pl-2 pr-2 pt-2">
         <App />
       </div>
-      <div className="row-start-5 row-end-7 col-start-1 col-end-5">
-        <Footer />
-      </div>
-      <div className="row-start-4 row-end-5 col-start-4 col-end-5 z-0">
-        <Coffee />
+      <div className="navbar">
+        <Navbar />
       </div>
     </div>
   );
