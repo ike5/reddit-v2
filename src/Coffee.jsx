7a31d09cd29ad6a9f5659b471ed76eabce6778dc
Ike Maldonado <maldonadoike@gmail.com>
2022-12-01 14:08:42 -0800

    add coffee

---
+++ src/Coffee.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+function Coffee() {
+  return (
+    <a href="https://www.buymeacoffee.com/ike5">
+      <img src="https://img.buymeacoffee.com/button-api/?text=cups&emoji=&slug=ike5&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" />
+    </a>
+  );
+}
+
+export default Coffee;
--- src/Grid.jsx
+++ src/Grid.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import App from "./App";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Coffee from "./Coffee";
 
 function Grid() {
   return (
-    <div className="grid auto-rows-fr grid-cols-4 gap-4">
-      {/* <div className="nav row-span-1 col-start-1 col-end-5">
-        <Navbar />
-      </div> */}
-      <div className="main row-span-4 col-start-1 col-end-5 pl-2 pr-2 pt-2">
+    <div className="grid grid-rows-6 grid-cols-4 gap-4 place-content-center">
+      <div className="row-start-1 row-end-5 col-start-1 col-end-5 pl-2 pr-2 pt-2">
         <App />
       </div>
-      <div className="footer row-span-1 col-start-1 col-end-5">
+      <div className="row-start-5 row-end-7 col-start-1 col-end-5">
         <Footer />
+      </div>
+      <div className="row-start-4 row-end-5 col-start-4 col-end-5 z-0">
+        <Coffee />
       </div>
     </div>
   );
