45843835915c4331efa0a0b66d3565fb1d22d2dc
Ike Maldonado <maldonadoike@gmail.com>
2022-12-01 22:08:17 -0800

    lower volume

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import App from "./App";
 import Instances from "./Instances";
 import Navbar from "./Navbar";
+import { Howl } from "howler";
 
 function Grid() {
   let audio_sky = new Howl({
@@ -10,12 +11,13 @@ function Grid() {
       "https://cdn.freesound.org/previews/477/477479_973833-lq.ogg",
     ],
     html5: true,
+    volume: 0.2,
   });
 
   return (
     <div className="my-container gap-4 place-content-center">
       <div className="navbar sticky top-0 z-50">
-        <Navbar audio={audio_sky}/>
+        <Navbar audio={audio_sky} />
       </div>
       <div className="main pl-2 pr-2">
         <App />
