c4aa18ec8f9bbc8a30ee9a34cc9cf6b23c15b420
Ike Maldonado <maldonadoike@gmail.com>
2022-12-04 18:07:09 -0800

    remove navbar

--- src/App.jsx
+++ src/App.jsx
@@ -161,7 +161,7 @@ export default function App() {
   return (
     <>
       <Canvas
-        className="rounded-lg drop-shadow-2xl"
+        className=""
         shadows
         camera={{ position: [-2, 0, 20], fov: 60 }}
       >
@@ -170,7 +170,7 @@ export default function App() {
         <ambientLight intensity={0.4} />
         <directionalLight intensity={0.7} castShadow position={[1, 3, 2]} />
         <Sky sunPosition={[1, 3, 2]} />
-        <Clouds />
+        {/* <Clouds /> */}
         <Suspense>
           <Physics gravity={[0, 0, 0]}>{rows}</Physics>
         </Suspense>
--- src/Grid.jsx
+++ src/Grid.jsx
@@ -17,10 +17,7 @@ function Grid() {
 
   return (
     <div className="my-container gap-4 place-content-center">
-      <div className="navbar sticky top-0 z-50">
-        <Navbar audio={audio_track} />
-      </div>
-      <div className="main pl-2 pr-2">
+      <div className="main">
         <App />
       </div>
     </div>
